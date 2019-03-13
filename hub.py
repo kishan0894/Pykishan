997983eba2a10d469f3356169329a9cb6b5d0de6
Kishan <kishan0894@gmail.com>
2019-03-13 18:43:52 +0530

    "commit 13/03/2019"

---
+++ hub.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Remote(command_executor='http://192.168.0.58:4444/wd/hub',
+                                       desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
+
+    def test_search_in_python_org(self):
+        driver=self.driver
+        driver.get("https://github.com")
+        assert "GitHub" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("dzitkowskik")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
