a88676e3815b26a317ad1c3efc208cfea472f2ef
Kishan <kishan0894@gmail.com>
2019-03-14 17:28:49 +0530

    selenium grid config with docker container

--- delete_docker_images.py
+++ delete_docker_images.py
@@ -1,28 +1,11 @@
-import os
-import re
-import commands
+import os;import re;import commands
 _, string =commands.getstatusoutput("docker images")
 string = re.sub(r'([^ ]) ([^ ])',r'\1_\2', string)
 lines = string.splitlines()    #Splitting Lines
 string.replace(' ','_', 0)
-columns = re.sub(' +', ' ', lines[0]).split() # removing all the spaces from first row creating a list named columns
-dict_of_list={} # created empty dictionary with elements of first row as keys
-for column in columns:
-    dict_of_list[column]=[]
-for line in range (2,len(lines)): #appending respective row's data to keys in dictionary
-    element =0
-    row = re.sub(' +', ' ', lines[line])
-    row = row.split()
-    for element in range (0, len(row)):
-        dict_of_list[columns[element]].append(row[element])
-        element+=1
-new_command='docker rmi '
-
-Image_id=dict_of_list['IMAGE_ID']
-for id in Image_id:
-    new_command += ' '
-    new_command += str(id)
-
-new_command += ' -f'
-
-os.system(new_command)
+new_command='docker rmi'
+for line in range (1,len(lines)): #appending respective row's data to keys in dictionary
+    row = re.sub(' +', ' ', lines[line]).split()
+    new_command+= ' ' + row[2]
+new_command+= ' -f'
+print(new_command)
