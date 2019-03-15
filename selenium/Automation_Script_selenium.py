d99aae852597549bdabbf75dda70cf34ca365f30
Kishan <kishan0894@gmail.com>
2019-03-15 13:54:32 +0530

    Grid setup on docker container

--- selenium/Automation_Script_selenium.py
+++ selenium/Automation_Script_selenium.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,15 +6,16 @@ from selenium.webdriver.common.keys import Keys
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
+        self.driver = webdriver.Remote(command_executor='http://172.17.0.5:5555/wd/hub',
+                                       desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
         #self.driver = webdriver.Remote(command_executor='http://192.168.0.58:4444/wd/hub',
-        #                               desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
-        self.driver = webdriver.Remote(command_executor='http://192.168.0.58:4444/wd/hub',
-                                       desired_capabilities=webdriver.DesiredCapabilities.CHROME)
+        #                               desired_capabilities=webdriver.DesiredCapabilities.CHROME)
 
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("https://github.com")
         assert "GitHub" in driver.title
+        time.sleep(30)
         elem = driver.find_element_by_name("q")
         elem.send_keys("dzitkowskik")
         elem.send_keys(Keys.RETURN)
